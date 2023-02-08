7f5e256b825ada72a178f9fe936ac7f582e88a86
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-02-08 11:48:11 +0600

    function default parameter for not provided values

---
+++ js/let-const.js
@@ -0,0 +1,12 @@
+// breakup with var
+// no more use of var
+// let: let it to reassign
+// const: do not allow it to reassign
+
+let money = 25;
+money = 35;
+console.log(money);
+
+const bird = 'jan pakhi';
+bird = 'moyna pakhi';
+console.log(bird)
