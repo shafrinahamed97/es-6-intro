643ad8a30b120ec79ed53ef243b2987b58c240c3
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-02-08 12:33:09 +0600

    template string, multiple line string, dynamic string

---
+++ js/template.js
@@ -0,0 +1,20 @@
+const person = 'Adam Sand';
+const person2 = "Ben White";
+const person3 = `Donal Trump`;
+
+const multiLine = `
+First Line of Text
+Second Line of Text
+Third Line of String
+Fourth Line of Code;
+`
+// console.log(multiLine);
+
+const a = 20;
+const b = 30;
+const nums = [78, 98, 45, 12, 9, 444]
+const summary = 'sum of ' + a + ' ' + ' and ' + b + ' is ' + (a + b)
+// console.log(summary);
+
+const newSummary = `sum of ${a} and ${b} is: ${a + b}`;
+console.log(newSummary);
